d4686e2dd448987c9ea0830ad740ddf470459587
stephenm05 <stephen.mccollum@maine.edu>
2025-04-16 23:31:21 -0400

    Added initial function definitions

--- player.h
+++ player.h
@@ -8,10 +8,25 @@ typedef struct Player Player;
 
 struct Player{
     char name[MAX_LINE_LENGTH];
-    struct Card* cards;
+    struct Card* draws;
     int wrongGuesses;
     Player* nextPlayer;
+    int drawCount;
+    int maxDraws;
 };
+
+//function to make a Player
+Player* makePlayer(char* n,Player* np);
+
+//function to make the first Player struct (since for the first one we won't have a Player* argument to put in)
+Player* makeFirstPlayer(char* n);
+
+//function to free a Player struct
+//takes a pointer to the Player pointer you actually want to free and set to NULL
+void freePlayer(Player** p);
+
+//function to add the Card pointed to by c to p->draws, expanding the latter as needed
+int drawCard(Player* p,Card* c);
 
 #endif
 
