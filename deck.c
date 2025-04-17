d0f3345926c3a73e320b30478514126a1f964266
stephenm05 <stephen.mccollum@maine.edu>
2025-04-17 00:29:32 -0400

    Defined makeDeck()

--- deck.c
+++ deck.c
@@ -8,7 +8,23 @@
 //define functions declared in header file here
 
 //function to create a Deck struct
-Deck* makeDeck();
+Deck* makeDeck() {
+    //allocate space for the Deck struct
+    Deck* d = malloc(sizeof(Deck));
+    //set simple initial member values
+    (d->timesShuffled)=0;
+    //create a full deck's worth of Card structs and add their pointers to d->cardOrder
+    //outer loop goes through the suits and inner loop goes through the ranks, so the starting order is all clubs, then all diamonds, all spades, and all hearts in ascending order
+    int index=0;
+    for(int s=1;s<=4;s++) {
+        for(int r=1;r<=13;r++) {
+            (d->cardOrder)[index]=makeCard(s,r);
+            index++;
+        }
+    }
+    //return a pointer to the Deck created so the user can interact with it
+    return d;
+}
 
 //function to shuffle the Deck
 void shuffleDeck(Deck* d);
